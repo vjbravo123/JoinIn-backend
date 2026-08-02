3168df87117ff46e0e22582e0248957c1b6a391b
vjbravo123 <vjbravojoshi@gmail.com>
2026-08-02 18:38:59 +0530

    fixed again

--- src/modules/activities/activities.service.ts
+++ src/modules/activities/activities.service.ts
@@ -51,68 +51,70 @@ export class ActivitiesService {
   }
 
   async joinActivity(userId: string, activityId: string) {
-    const activity = await this.activityModel.findById(activityId);
-    if (!activity) throw new BadRequestException('Activity not found');
+  const activity = await this.activityModel.findById(activityId);
+  if (!activity) throw new BadRequestException('Activity not found');
 
-    const userObjectId = new Types.ObjectId(userId);
+  const userObjectId = new Types.ObjectId(userId);
 
-    // 1. Prevent joining if user already joined
-    if (activity.participants.some((id) => id.equals(userObjectId))) {
-      throw new BadRequestException('You already joined this activity');
-    }
-
-    // 2. Prevent joining if activity is full
-    if (activity.participants.length >= activity.maxParticipants) {
-      throw new BadRequestException('Activity is full');
-    }
-
-    // 3. Add user to Activity Participants
-    activity.participants.push(userObjectId);
-    await activity.save();
-
-    // 4. Check if a GROUP chatroom exists for this activity
-    let chatRoom = await this.chatRoomModel.findOne({
-      activity: activity._id,
-      type: 'GROUP',
-    });
-
-    if (!chatRoom) {
-      // If group chatroom does not exist, create it and add ALL participants (host + all current joined members)
-      const uniqueMembers = Array.from(
-        new Set([
-          activity.host.toString(),
-          ...activity.participants.map((p) => p.toString()),
-        ]),
-      ).map((id) => new Types.ObjectId(id));
-
-      chatRoom = await this.chatRoomModel.create({
-        type: 'GROUP',
-        activity: activity._id,
-        members: uniqueMembers,
-      });
-    } else {
-      // If group chatroom exists, add the newly joined user to members
-      await this.chatRoomModel.updateOne(
-        { _id: chatRoom._id },
-        { $addToSet: { members: userObjectId } },
-      );
-    }
-
-    // 5. Fetch populated activity details to return to the frontend
-// 5. Fetch populated activity details to return to the frontend
-    const updatedActivity = await this.activityModel
-      .findById(activityId)
-      .populate('host', 'name avatar isPhoneVerified')
-      .exec();
-
-    if (!updatedActivity) {
-      throw new BadRequestException('Activity not found');
-    }
-
-    // Return the updated activity object with chatRoomId included
-    return {
-      ...updatedActivity.toObject(),
-      chatRoomId: chatRoom._id,
-    };
+  // 1. Prevent joining if user already joined
+  if (activity.participants.some((id) => id.equals(userObjectId))) {
+    throw new BadRequestException('You already joined this activity');
   }
+
+  // 2. Prevent joining if activity is full
+  if (activity.participants.length >= activity.maxParticipants) {
+    throw new BadRequestException('Activity is full');
+  }
+
+  // 3. ATOMIC UPDATE: Add user to Activity Participants using $addToSet
+  // This avoids calling .save() which triggers full schema re-validation
+  await this.activityModel.updateOne(
+    { _id: activityId },
+    { $addToSet: { participants: userObjectId } }
+  );
+
+  // 4. Check if a GROUP chatroom exists for this activity
+  let chatRoom = await this.chatRoomModel.findOne({
+    activity: activity._id,
+    type: 'GROUP',
+  });
+
+  if (!chatRoom) {
+    // Collect unique member IDs cleanly
+    const existingHostId = activity.host ? activity.host.toString() : userId;
+    const existingParticipants = activity.participants.map((p) => p.toString());
+    
+    const uniqueMembers = Array.from(
+      new Set([existingHostId, ...existingParticipants, userId]),
+    ).map((id) => new Types.ObjectId(id));
+
+    chatRoom = await this.chatRoomModel.create({
+      type: 'GROUP',
+      activity: activity._id,
+      members: uniqueMembers,
+    });
+  } else {
+    // Add newly joined user to group chat members
+    await this.chatRoomModel.updateOne(
+      { _id: chatRoom._id },
+      { $addToSet: { members: userObjectId } },
+    );
+  }
+
+  // 5. Fetch populated activity details to return to the frontend
+  const updatedActivity = await this.activityModel
+    .findById(activityId)
+    .populate('host', 'name avatar isPhoneVerified')
+    .exec();
+
+  if (!updatedActivity) {
+    throw new BadRequestException('Activity not found');
+  }
+
+  // Return the updated activity object with chatRoomId included
+  return {
+    ...updatedActivity.toObject(),
+    chatRoomId: chatRoom._id,
+  };
+}
 }
