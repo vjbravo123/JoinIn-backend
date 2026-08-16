aad1fa98efcd57e0a683734403e19206249e695b
vjbravo123 <vjbravojoshi@gmail.com>
2026-08-16 16:21:06 +0530

    acitivites locations fixed

--- src/modules/activities/schemas/activity.schema.ts
+++ src/modules/activities/schemas/activity.schema.ts
@@ -6,38 +6,56 @@ export type ActivityDocument = Activity & Document;
 @Schema({ timestamps: true })
 export class Activity {
   @Prop({ required: true })
-  title: string;
+  title!: string;
 
   @Prop({ required: true })
-  description: string;
+  description!: string;
 
   @Prop({ required: true })
-  category: string;
+  category!: string;
 
   @Prop({ required: true })
-  locationName: string;
+  locationName!: string;
 
   @Prop({ required: true })
-  latitude: number;
+  latitude!: number;
 
   @Prop({ required: true })
-  longitude: number;
+  longitude!: number;
+
+  @Prop({
+    type: {
+      type: String,
+      enum: ['Point'],
+      default: 'Point',
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  })
+  location!: {
+    type: string;
+    coordinates: number[];
+  };
 
   @Prop({ required: true })
-  eventDate: Date;
+  eventDate!: Date;
 
   @Prop({ default: 10 })
-  maxParticipants: number;
+  maxParticipants!: number;
 
-  // Added images field to store array of image URLs
   @Prop({ type: [String], default: [] })
-  images: string[];
+  images!: string[];
 
   @Prop({ type: Types.ObjectId, ref: 'User', required: true })
-  host: Types.ObjectId;
+  host!: Types.ObjectId;
 
   @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], default: [] })
-  participants: Types.ObjectId[];
+  participants!: Types.ObjectId[];
 }
 
 export const ActivitySchema = SchemaFactory.createForClass(Activity);
+
+// Required for $geoNear / $near geospatial queries
+ActivitySchema.index({ location: '2dsphere' });
